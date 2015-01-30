ff03b85b457bff8aa7e3bbbf1ee8c6ef323ca46c
radosuav <rmgu@dhi-gras.com>
2015-01-30 11:17:03 +0100

    Make sure that Toolbox GUI is updated after workflows are loaded.

--- ProcessingWorkflowPlugin.py
+++ ProcessingWorkflowPlugin.py
@@ -58,10 +58,19 @@ class ProcessingWorkflowPlugin:
 class WorkflowAlgListListner():
     def __init__(self, workflowProvider):
         self.workflowProvider = workflowProvider
+        self.recursiveCall = False
 			
     def algsListHasChanged(self):
+        if self.recursiveCall:
+            return
         self.workflowProvider.createAlgsList()
         algs = {}
         for alg in self.workflowProvider.preloadedAlgs:
             algs[alg.commandLineName()] = alg
         Processing.algs[self.workflowProvider.getName()] = algs
+        
+        # fireAlgsListHasChanged to update the Toolbox GUI but make sure that the 
+        # call doesn't lead to infinite loop
+        self.recursiveCall = True
+        Processing.fireAlgsListHasChanged()
+        self.recursiveCall = False
