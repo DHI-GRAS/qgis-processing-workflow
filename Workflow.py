2e548459f2237c1ac8eb9652fddc73cdecf8858d
Jonas Sølvsteen <j08lue@gmail.com>
2018-01-24 13:35:20 +0100

    Derive window title from provider

--- Workflow.py
+++ Workflow.py
@@ -16,7 +16,7 @@
 * by the Free Software Foundation, either version 3 of the License,       *
 * or (at your option) any later version.                                  *
 *                                                                         *
-* WOIS is distributed in the hope that it will be useful, but WITHOUT ANY * 
+* WOIS is distributed in the hope that it will be useful, but WITHOUT ANY *
 * WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
 * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License   *
 * for more details.                                                       *
@@ -43,8 +43,8 @@ from processing_workflow.StepDialog import StepDialog, NORMAL_MODE, BATCH_MODE
 from processing_workflow.WrongWorkflowException import WrongWorkflowException
 from processing_workflow.WorkflowUtils import WorkflowUtils
 
-# Class containing the list of steps (algorithms) in the workflow together with the mode 
-# and instructions for each step 
+# Class containing the list of steps (algorithms) in the workflow together with the mode
+# and instructions for each step
 class Workflow(GeoAlgorithm):
 
     def __init__(self, provider):
@@ -57,58 +57,58 @@ class Workflow(GeoAlgorithm):
         self.descriptionFile = None
         self.parameters = [ParameterString("Info", "Workflow can not be run as a batch process. Please close this dialog and execute as a normal process.", "", False)]
         self.showInModeler = False
-        
+
     def addStep(self, algorithm, mode, instructions, algParameters = {}):
         step = {'algorithm' : algorithm, 'mode' : mode, 'instructions' : instructions, 'parameters' : algParameters}
         self._steps.append(step)
-            
+
     def changeStep(self, index, algorithm, mode, instructions, algParameters = {}):
         algParameters = {}
         for param in algorithm.parameters:
             if isinstance(param, ParameterBoolean) or isinstance(param, ParameterNumber) or isinstance(param, ParameterString) or isinstance(param, ParameterSelection) or isinstance(param, ParameterExtent):
                 algParameters[param.name] = param.value
         self._steps[index] = {'algorithm' : algorithm, 'mode' : mode, 'instructions' : instructions, 'parameters' : algParameters}
-    
+
     def changeMode(self, index, mode):
         self._steps[index]['mode'] = mode
-        
+
     def changeInstructions(self, index, instructions):
         self._steps[index]['instructions'] = instructions
-        
+
     def getLength(self):
         return len(self._steps)
-    
+
     def getAlgorithm(self, index):
         return self._steps[index]['algorithm']
-    
+
     def getMode(self, index):
         return self._steps[index]['mode']
-    
+
     def getInstructions(self, index):
-        return self._steps[index]['instructions']    
-    
+        return self._steps[index]['instructions']
+
     def getIcon(self):
         try:
             return self.provider.getIcon()
         except:
             return  WorkflowUtils.workflowIcon()
-    
+
     def getCopy(self):
         newone = Workflow(self.provider)
         newone.openWorkflow(self.descriptionFile)
         return newone
-    
+
     def getCustomParametersDialog(self):
         return WorkflowDialog(self)
-        
+
     def removeStep(self, index):
         self._steps.pop(index)
-        
-    def run(self):    
+
+    def run(self):
         # execute the first step
         step = self._steps[0]
         stepDialog = self.executeStep(step)
-        
+
         # execute the rest
         while True:
             # check if workflow should go forward, backward or finish
@@ -118,14 +118,14 @@ class Workflow(GeoAlgorithm):
                 step = self.previousStep(step)
             else:
                 step = None
-            
-            # finish the workflow or execute the next step 
+
+            # finish the workflow or execute the next step
             if step == None:
-                GrassUtils.endGrassSession() 
+                GrassUtils.endGrassSession()
                 return stepDialog.executed
-            else:        
-                stepDialog = self.executeStep(step)        
-    
+            else:
+                stepDialog = self.executeStep(step)
+
     def setStepParameters(self, step):
         parameters = step['parameters']
         alg = step['algorithm']
@@ -133,37 +133,43 @@ class Workflow(GeoAlgorithm):
             param = alg.getParameterFromName(paramName)
             if param and (isinstance(param, ParameterBoolean) or isinstance(param, ParameterNumber) or isinstance(param, ParameterString) or isinstance(param, ParameterSelection) or isinstance(param, ParameterExtent)):
                 param.default = parameters[paramName]
-    
+
     def executeStep(self, step):
         if isinstance(step['algorithm'], GrassAlgorithm):
             GrassUtils.startGrassSession()
         else:
-            GrassUtils.endGrassSession() 
-        self.setStepParameters(step)    
+            GrassUtils.endGrassSession()
+        self.setStepParameters(step)
         stepDialog = StepDialog(step['algorithm'], None, os.path.dirname(self.descriptionFile), False)
-        stepDialog.setMode(step['mode']) 
-        stepDialog.setInstructions(step['instructions']) 
-        stepDialog.setWindowTitle("WOIS Workflow " + unicode(self.name) + ", Step "+unicode(self._steps.index(step)+1)+" of "+unicode(len(self._steps))+": "+step['algorithm'].name)
+        stepDialog.setMode(step['mode'])
+        stepDialog.setInstructions(step['instructions'])
+        stepDialog.setWindowTitle(
+            "{self.provider.name} {self.name}, Step {stepno} of {nsteps}: {algname}"
+            .format(
+                self=self,
+                stepno=(self._steps.index(step) + 1),
+                nsteps=len(self._steps),
+                algname=step['algorithm'].name))
         stepDialog.setWindowIcon(self.getIcon())
         # set as window modal to allow access to QGIS functions
         stepDialog.setWindowModality(1)
         stepDialog.exec_()
         return stepDialog
-        
+
     def nextStep(self, step):
         index = self._steps.index(step)
         if index < len(self._steps)-1:
             return (self._steps[index+1])
         else:
             return None
-            
+
     def previousStep(self, step):
         index = self._steps.index(step)
         if index > 0:
-            return (self._steps[index-1]) 
+            return (self._steps[index-1])
         else:
-            return (self._steps[0])  
-            
+            return (self._steps[0])
+
     def serialize(self):
         s=".NAME:" + unicode(self.name) + "\n"
         s +=".GROUP:" + unicode(self.group) + "\n"
@@ -174,11 +180,11 @@ class Workflow(GeoAlgorithm):
             s += ".MODE:" + step['mode'] + "\n"
             s += ".INSTRUCTIONS:" + step['instructions']
             if not unicode(s).endswith("\n"):
-                s+="\n" 
+                s+="\n"
             s +="!INSTRUCTIONS" + "\n"
-        
+
         return s
-    
+
     # Read workflow from text file
     def openWorkflow(self, filename):
         self._steps = list()
@@ -187,17 +193,17 @@ class Workflow(GeoAlgorithm):
         try:
             for line in fileinput.input(filename, openhook = fileinput.hook_encoded("utf-8")):
                 line= line.rstrip()
-                
+
                 # comment line
                 if line.startswith("#"):
                     pass
-                
+
                 if line.startswith(".NAME:"):
                     self.name = line[len(".NAME:"):]
-                    
+
                 elif line.startswith(".GROUP:"):
                     self.group = line[len(".GROUP:"):]
-                    
+
                 elif line.startswith(".ALGORITHM:"):
                     alg = Processing.getAlgorithm(line[len(".ALGORITHM:"):])
                     if alg:
@@ -205,15 +211,15 @@ class Workflow(GeoAlgorithm):
                         self.addStep(alg, NORMAL_MODE, '')
                     else:
                         raise(WrongWorkflowException())
-                    
+
                 elif line.startswith(".MODE:"):
                     if line[len(".MODE:"):] == NORMAL_MODE:
                         self._steps[-1]['mode'] = NORMAL_MODE
                     elif line[len(".MODE:"):] == BATCH_MODE:
                         self._steps[-1]['mode'] = BATCH_MODE
                     else:
-                        raise(WrongWorkflowException())  
-                
+                        raise(WrongWorkflowException())
+
                 elif line.startswith(".PARAMETERS:"):
                     try:
                         params = json.loads(line[len(".PARAMETERS:"):])
@@ -225,12 +231,12 @@ class Workflow(GeoAlgorithm):
                             self.setStepParameters(self._steps[-1])
                         else:
                             raise(WrongWorkflowException())
-                      
+
                 elif line.startswith(".INSTRUCTIONS"):
                     instructions = line[len(".INSTRUCTIONS:"):]+"\n"
                     self._steps[-1]['instructions'] = instructions
                     instructions = True
-                
+
                 elif instructions:
                     if line == "!INSTRUCTIONS":
                         instructions = False
@@ -246,22 +252,22 @@ class Workflow(GeoAlgorithm):
         except Exception, e:
             fileinput.close()
             raise e
-            
+
         fileinput.close()
-        
+
     def processAlgorithm(self, progress):
         self.run()
-        
-        
-# Just a "wrapper" dialog to satisfy executeAlgorithm in ProcessingToolbox       
+
+
+# Just a "wrapper" dialog to satisfy executeAlgorithm in ProcessingToolbox
 class WorkflowDialog(QtGui.QDialog):
-    
+
     def __init__(self, workflow):
         self.executed = workflow.run()
-               
-    def exec_(self):    
+
+    def exec_(self):
         None
-        
+
     def show(self):
         None
-        
+
